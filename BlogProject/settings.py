bec32a56bf34ce70fbe1f3b34162fc9d4d248424
niiick <1284849436@qq.com>
2021-10-02 14:14:01 +0800

    alpha v1.0.1

--- BlogProject/settings.py
+++ BlogProject/settings.py
@@ -28,7 +28,6 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', 'renns.com']
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -121,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
